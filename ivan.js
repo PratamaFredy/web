c9aa8a548e99e64bfbc691ff5dfd1b703fbc450e
PratamaFredy <86200371+PratamaFredy@users.noreply.github.com>
2021-06-21 12:09:48 +0700

    Update ivan.js

--- ivan.js
+++ ivan.js
@@ -15,11 +15,11 @@
   }
 
   function setup() {
-    canvas=createCanvas(320, 260);
+    canvas=createCanvas(300, 260);
       canvas.parent('cek');
     // Create the video
     video = createCapture(VIDEO);
-    video.size(320, 240);
+    video.size(300, 240);
     video.hide();
 
     flippedVideo = ml5.flipImage(video);
@@ -38,9 +38,7 @@
     textAlign(CENTER);
     text(label, width / 2, height - 4);
   }
-  function windowResized() {
-    resizeCanvas(320, 260);
-  }
+
   // Get a prediction for the current video frame
   function classifyVideo() {
     flippedVideo = ml5.flipImage(video)
